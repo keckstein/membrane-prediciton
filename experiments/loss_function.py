353894e391fc6914260bb9b27aacd12f48b23390
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-03-17 16:12:48 +0100

    loss function

--- experiments/loss_function.py
+++ experiments/loss_function.py
@@ -12,9 +12,10 @@ class WeightMatrixWeightedBCE(nn.Module):
     def forward(self, y_pred, y_gt):
 
         cw = self.class_weights
-        num_channels = y_pred.numpy().shape[1]
+        num_channels = y_pred.shape[1]
 
         assert len(cw) == num_channels, 'Class weight sets and number of channels have to match!'
+        _epsilon = 1e-7
         y_pred = t.clamp(y_pred, _epsilon, 1-_epsilon)
 
         w = y_gt[:,-1,:][:,None,:]
@@ -23,7 +24,7 @@ class WeightMatrixWeightedBCE(nn.Module):
             for c in range(num_channels):
                 y_gt_c = y_gt[:,c,:][:,None,:]
                 y_pred_c = y_pred[:,c,:][:,None,:]
-                loss += w * -(cw[c][1] * y_gt_c * t.log(y_pred_c) + cw[c][0] * (1.0-y_gt_g) * t.log(-y_pred_c + 1.0))
+                loss += w * -(cw[c][1] * y_gt_c * t.log(y_pred_c) + cw[c][0] * (1.0-y_gt_c) * t.log(-y_pred_c + 1.0))
 
         else:
             for c in range(num_channels):
