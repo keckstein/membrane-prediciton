5ebb76d9c3485e039c291b0f0d3c898dfa58e349
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-21 15:35:12 +0200

    piled unet 14

--- experiments/piled_unet_14_2_cluster.py
+++ experiments/piled_unet_14_2_cluster.py
@@ -76,7 +76,7 @@ train_gen = parallel_data_generator(
     raw_channels=[[raw_3[:, :, 357:772]]],
     gt_channels=[[gt_3[:, :, 357:772], mask_3[:, :, 357:772]]],
     spacing=(32,32,32),  # (32, 32, 32),  For testing, I increased the grid spacing, speeds things up for now
-    area_size=[(raw_3[0:32, :, 357:772]).shape],
+    area_size=[(raw_3[0:64, :, 357:772]).shape],
     # Can now be a tuple of a shape for each input volume        areas_and_spacings=None,
     target_shape=(64, 64, 64),
     gt_target_shape=(64, 64, 64),
@@ -183,7 +183,7 @@ network.train()
 
 # tensorboard
 # example_input = torch.rand(1, 1, 64, 64, 64)
-writer = SummaryWriter('/g/schwab/eckstein/scripts/tensorboard/piled_unet_14_run2_test_area_size' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+writer = SummaryWriter('/g/schwab/eckstein/scripts/tensorboard/piled_unet_14_run2_displaced' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 # writer.add_graph(network, example_input, verbose=True)  # graph with network structure, verbose = True prints result
 # writer.flush()
 
@@ -318,7 +318,7 @@ for x, y, epoch, n, loe in train_gen:
                     # save model if val_loss is improved
                     if best_val_loss is None or val_loss < best_val_loss:
                         best_val_loss = val_loss
-                        torch.save(network.state_dict(), f'/scratch/eckstein/models/piled_unet_14_run2_test_area_size/result{epoch:04d}.h5')
+                        torch.save(network.state_dict(), f'/scratch/eckstein/models/piled_unet_14_run2_displaced/result{epoch:04d}.h5')
                     break
 
 writer.close()
