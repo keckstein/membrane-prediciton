699c77c7352db2eae3e1ea7d3b96d99a053898bf
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-22 20:44:03 +0200

    piled unet 22-28

--- experiments/piled_unet_24_2_cluster.py
+++ experiments/piled_unet_24_2_cluster.py
@@ -183,9 +183,12 @@ writer = SummaryWriter(
 optimizer = optim.Adam(network.parameters(), lr=0.0001, betas=(0.9, 0.999), weight_decay=1e-5, eps=1e-7)
 # define loss function
 
-loss = WeightMatrixWeightedBCE(0.2, 0.8) #WeightMatrixWeightedBCE(((0.4, 0.6),), weigh_with_matrix_sum=False), WeightMatrixWeightedBCE(((0.5, 0.5),), weigh_with_matrix_sum=False),,
-
-
+oss = CombinedLosses(losses=(
+    WeightMatrixWeightedBCE((0.2, 0.8), weigh_with_matrix_sum=False)), #WeightMatrixWeightedBCE(((0.4, 0.6),), weigh_with_matrix_sum=False), WeightMatrixWeightedBCE(((0.5, 0.5),), weigh_with_matrix_sum=False),,
+    y_pred_channels=(np.s_[0]),  # , np.s_[1:2], np.s_[2:3]
+    y_true_channels=(np.s_[:]),  # , np.s_[:]), np.s_[:]
+    weigh_losses=None  #0.6, 0.5
+)
 sum_train_loss = 0
 best_val_loss = None
 
