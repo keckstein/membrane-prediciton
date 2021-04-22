a481ff0ccaa3686fa049de8952bebab3b8e11b01
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-22 21:24:27 +0200

    piled unet 22-28

--- experiments/piled_unet_24_1_cluster.py
+++ experiments/piled_unet_24_1_cluster.py
@@ -184,7 +184,7 @@ optimizer = optim.Adam(network.parameters(), lr=0.0001, betas=(0.9, 0.999), weig
 
 loss = CombinedLosses(losses=(
     WeightMatrixWeightedBCE((0.2, 0.8), weigh_with_matrix_sum=False)), #WeightMatrixWeightedBCE(((0.4, 0.6),), weigh_with_matrix_sum=False), WeightMatrixWeightedBCE(((0.5, 0.5),), weigh_with_matrix_sum=False),,
-    y_pred_channels=(np.s_[1]), # , np.s_[1:2], np.s_[2:3]
+    y_pred_channels=(np.s_[0]), # , np.s_[1:2], np.s_[2:3]
     y_true_channels=(np.s_[:]),   # , np.s_[:]), np.s_[:]
     weigh_losses=[1]#0.6, 0.5
 )
