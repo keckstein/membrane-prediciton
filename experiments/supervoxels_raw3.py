f7758fc3c8df0df3b34d565649e2f12526313c15
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-28 18:40:41 +0200

    supervoxels

---
+++ experiments/supervoxels_raw3.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('/g/schwab/eckstein/code/membrane-prediciton/membranet_segmentation/scripts/')
+import os
+from run_supervoxels import run
+
+target_folder = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/supervoxels/'
+source_filepath = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/predictions/reshaped_prediction_raw_3_result0150.h5'
+target_filepath = f'{target_folder}raw_3_result0150_3.h5'
+source_in_file = 'data'
+
+#if not os.path.exists(target_folder:
+    #os.makedirs(target_folder)
+
+run(source_filepath,target_filepath,source_in_file,verbose = False)
+
