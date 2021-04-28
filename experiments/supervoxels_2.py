bd914229bd3ee739d370d31c00f1c49a2b0dd088
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-28 12:02:26 +0200

    supervoxels

--- experiments/supervoxels_2.py
+++ experiments/supervoxels_2.py
@@ -3,12 +3,12 @@ sys.path.append('/g/schwab/eckstein/code/membrane-prediciton/membranet_segmentat
 import os
 from run_supervoxels import run
 
-
+target_folder = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/supervoxels/'
 source_filepath = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/predictions/reshaped_prediction_32_c013_result0150.h5'
-target_filepath = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/supervoxels/c013_result0150.h5'
+target_filepath = f'{target_folder}c013_result0150.h5'
 source_in_file = 'data'
-if not os.path.exists(target_filepath):
-    os.makedirs(target_filepath)
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
 
 run(source_filepath,target_filepath,source_in_file,verbose = False)
 
