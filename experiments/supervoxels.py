36e892c958f9602cf9ab7195102e728a7a4c2545
keckstein <Katharina.Eckstein@stud.uni-heidelberg.de>
2021-04-28 11:33:44 +0200

    supervoxels

--- experiments/supervoxels.py
+++ experiments/supervoxels.py
@@ -7,7 +7,7 @@ from run_supervoxels import run
 source_filepath = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/predictions/reshaped_prediction_32_c013_result0150.h5'
 target_filepath = '/g/schwab/eckstein/code/models/unet3d_tomo/cluster/piled_unet_14_run2_new_gt/supervoxels/c013_result0150.h5'
 source_in_file = 'data'
-if not target_filepath:
+if not os.path.exists(target_filepath):
     os.makedirs(target_filepath)
 
 run(source_filepath,target_filepath,source_in_file,verbose = False)
